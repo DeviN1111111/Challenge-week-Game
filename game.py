b35f0429bae0c4068d3b5527a721c9bdce96b413
EyeMustache <lucioridder2003@gmail.com>
2024-09-30 13:48:17 +0200

    game send

--- game.py
+++ game.py
@@ -2,8 +2,9 @@ from functions import *
 
 def gameloop():
     print(main_menu())
-    test = input()
-    is_choice_valid(test,2)
+    picked_choice = input()
+    is_choice_valid(picked_choice,2)
+    input_error(is_choice_valid)
 
 
 gameloop()
