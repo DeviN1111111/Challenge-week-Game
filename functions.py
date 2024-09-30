60882108a9025c63bec646bb1cf7146677d9b36e
EyeMustache <lucioridder2003@gmail.com>
2024-09-30 15:54:59 +0200

    updates
    u were there

--- functions.py
+++ functions.py
@@ -26,20 +26,30 @@ def main_menu()->str:
 ========================================'''
 
 
-def is_choice_valid(picked_choice:str,total_choices:int):#->bool?():
-    if int(picked_choice) >= 1 and int(picked_choice) <= total_choices:
-        return True
+
+def valid_input(prompt: str, total_choices: int)-> str:
+    user_input = input(prompt)  
+    if user_input.isdigit():
+        choice = int(user_input)
+        if 1 <= choice <= total_choices:
+            return str(choice)
+        else:
+            print("ERROR: Choice out of option range.")
+            return valid_input(prompt, total_choices)
     else:
-       return loop_till_correct_input(total_choices)
+        print("ERROR: Invalid input. Please enter a number.")
+        return valid_input(prompt, total_choices)
     
 
-        
-    
-def loop_till_correct_input(choices:int):
-    print("Input error")
-    return is_choice_valid(input(), choices)
-    
-    
-    
+def is_name_valid(prompt: str)-> str:
+    name = input(prompt)
+    name = name.capitalize()
+    if name.isalpha() and len(name) <= 10:
+        return name
+    else:
+        print("ERROR: Invalid input.")
+        return is_name_valid(prompt)
     
 
+def level_one():
+    typewriter()
--- game.py
+++ game.py
@@ -1,10 +1,26 @@
 from functions import *
 
 def gameloop():
-    print(main_menu())
-    test = input()
-    is_choice_valid(test,4)
-    print("correct")
-
+    while True:
+        print(main_menu())
+        menu_choice = valid_input("Enter your choice here: ", 2)
+        if menu_choice == '2':
+            typewriter("Game shutting down...")
+            break
+        elif menu_choice == '1':
+            typewriter('The game begins...')
+            name = is_name_valid("Enter your name: ")
+            typewriter(f"Welcome {name}.")
+            time.sleep(1)
+            typewriter('''
+placeholder intro text
+''')
+            
+            
+            
+    
+    
+            
+        
 
 gameloop()
