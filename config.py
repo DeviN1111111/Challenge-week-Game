0340a9ff8de8143dbeb4637c6970c272559a3486
EyeMustache <lucioridder2003@gmail.com>
2024-10-01 12:07:16 +0200

    added combat, update config

--- config.py
+++ config.py
@@ -1,2 +1,19 @@
 time_sleep = [0, 0]
 
+weapons = [
+    {"name": "Fist", "damage": 20},
+    {"name": "Dagger", "damage": 20},
+    {"name": "Bow", "damage": 40}
+]
+
+player_stats = {
+    "name": "no_name_entered",
+    "health": 100,
+    "weapon": "Fist",
+}
+
+enemy_stats = {
+    "Zombie": {"damage": 50, "health": 50},
+    "Skeleton": {"damage": 7, "health": 40},
+    "Vampire": {"damage": 10, "health": 60}
+}
--- functions.py
+++ functions.py
@@ -1,6 +1,6 @@
 import time
 import random
-from config import time_sleep
+from config import *
 
 
 def typewriter(str):
@@ -8,26 +8,6 @@ def typewriter(str):
         time.sleep(random.uniform(time_sleep[0], time_sleep[1]))
         print(letter, end='', flush = True)
     print()
-
-
-def main_menu()->str:
-    return'''
-========================================
-        Welcome to placeholder
-        made by Devin & Lucio
-========================================
-In a world overrun by plague and creatures of darkness, your wife lies gravely ill. 
-As a skilled alchemist, you alone have the knowledge to craft a cure,
-but the rare ingredients you need are scattered across a dying, perilous land.
-Armed with potions and desperation, you must venture beyond the safety of your lab
-into forgotten ruins, haunted forests, and crumbling villages.
-Time is running out. Will you find the cure before it's too late?
-Or will the horrors of this shattered world consume you?
-========================================
-    Enter '1' to start the game.
-    Enter '2' to quit.
-========================================'''
-
 
 
 def valid_input(prompt: str, total_choices: int)-> str:
@@ -53,13 +33,55 @@ def is_name_valid(prompt: str)-> str:
         print("ERROR: Invalid input. Please enter letters only.")
         return is_name_valid(prompt)
     
+    
+def give_player_weapon(weapon_name:str):
+    for weapon in weapons:
+        if weapon["name"] == weapon_name:
+            player_stats["weapon"] = weapon
+            return
+    
+    print(f"Wapen {weapon_name} niet gevonden.")
+    
+def is_yesno_valid(prompt: str)-> str:
+    yesno = input(prompt)
+    yesno = yesno[0].capitalize() + yesno[1:].lower()
+    if yesno == 'Yes' or yesno == 'No':
+        return yesno
+    else:
+        print("ERROR: Invalid input. Please enter 'Yes or 'No'.")
+        return is_yesno_valid(prompt)
 
-def level_one():
-    typewriter('''
-As you step out of your lab, the air is thick with decay, and the once-familiar village now feels like a stranger.
-The sky, a bleak gray, hangs over crumbling houses and the echo of distant groans.
-With your satchel of alchemical supplies strapped tight, you head towards the town square, knowing the first ingredient lies somewhere within the village.
-But you don't make it far before a chilling sound stops you in your tracks.
-A shambling figure emerges from behind a broken cart—its skin gray, eyes lifeless. It stumbles toward you, hunger in its eyes. 
-You've read about these creatures, but now you face one, alone.
-Your hands shake as you ready your weapon. It's time to fight. Will your alchemy save you?''')
+
+def item_highlight(text: str):
+    return f"\033[91m{text}\033[0m"
+
+
+def combat(enemy:str):
+    if enemy in enemy_stats and player_stats["health"] > 0:
+        enemy_name = enemy
+        enemy = enemy_stats[enemy]
+        player_weapon = player_stats["weapon"]
+        enemy_hp_reset = enemy["health"]
+        typewriter(f"You encountered a {enemy_name} he has {enemy["health"]} HP and you have {player_stats["health"]} hp!")
+        
+        while player_stats["health"] > 0 and enemy["health"] > 0:
+            enemy["health"] -= player_weapon["damage"]
+            typewriter(f"You hit the {enemy_name} for {player_weapon['damage']} damage.")
+            if enemy["health"] > 0:
+                typewriter(f"{enemy_name} health is now {enemy['health']}.")
+            else:
+                typewriter(f"{enemy_name} has 0 HP he died")
+                enemy["health"] = enemy_hp_reset
+                return
+            if enemy["health"] > 0:    
+                player_stats["health"] -= enemy["damage"]
+                typewriter(f"The {enemy_name} hits you for {enemy['damage']} damage.")
+                typewriter(f"Your health is now {player_stats['health']}.")
+            if player_stats["health"] <= 0:
+                typewriter("You died")
+                break 
+    else:
+        typewriter(f"ERROR Enemy typo of je begint je gevecht met 0 of minder HP")
+        
+    
+        
